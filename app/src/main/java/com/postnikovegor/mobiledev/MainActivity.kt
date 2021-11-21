336e24cb261e4d270123bbc708acb14f25e7da64
Egor Postnikov <ea-post134@yandex.ru>
2021-11-21 05:51:54 +0300

    Feature-18: Add data and picture loading

--- app/src/main/java/com/postnikovegor/mobiledev/MainActivity.kt
+++ app/src/main/java/com/postnikovegor/mobiledev/MainActivity.kt
@@ -3,8 +3,20 @@ package com.postnikovegor.mobiledev
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.postnikovegor.mobiledev.databinding.ActivityMainBinding
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,28 +24,50 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val adapter = UserAdapter()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.apply {
-            setContentView(this.root)
-            usersRecyclerView.apply {
-                layoutManager = LinearLayoutManager(
-                    this@MainActivity,
-                    LinearLayoutManager.VERTICAL, false
-                )
-                this.adapter = adapter
+        setContentView(binding.root)
+
+        val adapter = setupRecyclerView(binding.usersRecyclerView)
+        binding.usersRecyclerView.isVisible = false
+        binding.progressBar.isVisible = true
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                adapter.userList = loadUsers()
+                adapter.notifyDataSetChanged()
+                binding.usersRecyclerView.isVisible = true
+                binding.progressBar.isVisible = false
             }
         }
-
-        adapter.userList = loadUsers()
-        adapter.notifyDataSetChanged()
     }
 
-    private fun loadUsers(): List<User> {
-        val list = mutableListOf<User>()
-        for (i in 0..10)
-            list.add(User("$i", "$i user", "$i group"))
-        return list
+    private fun setupRecyclerView(recyclerView: RecyclerView): UserAdapter {
+        val adapter = UserAdapter()
+        recyclerView.adapter = adapter
+        return adapter
+    }
+
+    private suspend fun loadUsers(): List<User> {
+        return withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+            provideApi().getUsers().data
+        }
+    }
+
+    private fun provideApi(): Api {
+        return Retrofit.Builder()
+            .client(provideOkHttpClient())
+            .baseUrl("https://reqres.in/api/")
+            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
+            .build()
+            .create(Api::class.java)
+    }
+
+    private fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    private fun provideMoshi(): Moshi {
+        return Moshi.Builder().build()
     }
 }
