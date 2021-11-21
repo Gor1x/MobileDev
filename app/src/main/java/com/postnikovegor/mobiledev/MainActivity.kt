7a0d07b1c3d745d3155ddcf9bdc444bba8f3fc1c
Egor Postnikov <ea-post134@yandex.ru>
2021-11-21 23:57:59 +0300

    Feature-18: Add viewModel

--- app/src/main/java/com/postnikovegor/mobiledev/MainActivity.kt
+++ app/src/main/java/com/postnikovegor/mobiledev/MainActivity.kt
@@ -2,41 +2,52 @@ package com.postnikovegor.mobiledev
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.postnikovegor.mobiledev.databinding.ActivityMainBinding
-import com.squareup.moshi.Moshi
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+    private val binding by viewBinding(ActivityMainBinding::bind)
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         val adapter = setupRecyclerView(binding.usersRecyclerView)
-        binding.usersRecyclerView.isVisible = false
-        binding.progressBar.isVisible = true
 
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                adapter.userList = loadUsers()
-                adapter.notifyDataSetChanged()
+                viewModel.viewState.collect { viewState ->
+                    renderViewState(viewState)
+                }
+            }
+        }
+    }
+
+    private fun renderViewState(viewState: MainViewModel.ViewState) {
+        when (viewState) {
+            is MainViewModel.ViewState.Data -> {
+                (binding.usersRecyclerView.adapter as UserAdapter).apply {
+                    userList = viewState.userList
+                    notifyDataSetChanged()
+                }
                 binding.usersRecyclerView.isVisible = true
                 binding.progressBar.isVisible = false
+            }
+            is MainViewModel.ViewState.Loading -> {
+                binding.usersRecyclerView.isVisible = false
+                binding.progressBar.isVisible = true
             }
         }
     }
@@ -45,29 +56,5 @@ class MainActivity : AppCompatActivity() {
         val adapter = UserAdapter()
         recyclerView.adapter = adapter
         return adapter
-    }
-
-    private suspend fun loadUsers(): List<User> {
-        return withContext(Dispatchers.IO) {
-            Thread.sleep(1000)
-            provideApi().getUsers().data
-        }
-    }
-
-    private fun provideApi(): Api {
-        return Retrofit.Builder()
-            .client(provideOkHttpClient())
-            .baseUrl("https://reqres.in/api/")
-            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
-            .build()
-            .create(Api::class.java)
-    }
-
-    private fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder().build()
-    }
-
-    private fun provideMoshi(): Moshi {
-        return Moshi.Builder().build()
     }
 }
