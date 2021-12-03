1ffede5a44ff94d501c3eb5ab8e22131d2fa3cd9
Egor Postnikov <ea-post134@yandex.ru>
2021-12-03 06:44:45 +0300

    Feature-26: Add sign-up logic and layout

--- app/src/main/java/com/postnikovegor/mobiledev/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/postnikovegor/mobiledev/ui/signup/SignUpViewModel.kt
@@ -1,6 +1,41 @@
 package com.postnikovegor.mobiledev.ui.signup
 
+import androidx.lifecycle.viewModelScope
+import com.postnikovegor.mobiledev.entity.UserRegistrationData
+import com.postnikovegor.mobiledev.repository.AuthRepository
 import com.postnikovegor.mobiledev.ui.base.BaseViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class SignUpViewModel : BaseViewModel() {
+
+    private val _eventChannel = Channel<Event>(Channel.BUFFERED)
+
+    fun eventsFlow(): Flow<Event> {
+        return _eventChannel.receiveAsFlow()
+    }
+
+    sealed class Event {
+        object SignUpSuccess : Event()
+        object SignUpEmailConfirmationRequired : Event()
+    }
+
+    fun signUp(
+        userRegistrationData: UserRegistrationData
+    ) {
+        viewModelScope.launch {
+            try {
+                AuthRepository.signUp(userRegistrationData)
+                _eventChannel.send(Event.SignUpEmailConfirmationRequired)
+
+                // TODO: Commented for debug purposes
+                //_eventChannel.send(Event.SignUpSuccess)
+
+            } catch (error: Exception) {
+                _eventChannel.send(Event.SignUpEmailConfirmationRequired)
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/postnikovegor/mobiledev/util/EditTextExtensions.kt
@@ -0,0 +1,5 @@
+package com.postnikovegor.mobiledev.util
+
+import android.widget.EditText
+
+fun EditText.getString() = text?.toString() ?: ""
