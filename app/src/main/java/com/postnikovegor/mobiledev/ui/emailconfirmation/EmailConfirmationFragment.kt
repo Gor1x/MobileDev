63a8423c1eac4dde137c0064daadb75693e6e824
Egor Postnikov <ea-post134@yandex.ru>
2021-12-06 20:56:57 +0300

    Feature-30: Add verification code timer

--- app/src/main/java/com/postnikovegor/mobiledev/ui/emailconfirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/postnikovegor/mobiledev/ui/emailconfirmation/EmailConfirmationFragment.kt
@@ -2,12 +2,21 @@ package com.postnikovegor.mobiledev.ui.emailconfirmation
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.postnikovegor.mobiledev.R
 import com.postnikovegor.mobiledev.databinding.FragmentEmailConfirmationBinding
 import com.postnikovegor.mobiledev.ui.base.BaseFragment
+import com.postnikovegor.mobiledev.ui.emailconfirmation.EmailConfirmationViewModel.ResendButtonState
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmation) {
 
@@ -20,7 +29,46 @@ class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmat
         viewBinding.backButton.setOnClickListener {
             findNavController().popBackStack()
         }
+        subscribeToResendButtonState()
+        viewBinding.repeatCodeButton.setOnClickListener {
+            viewModel.notifyResendCodeButtonClicked()
+        }
     }
 
+    private fun subscribeToResendButtonState() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.resendButtonStateFlow.collect { state ->
+                    withContext(Main) {
+                        renderButtonState(state)
+                    }
+                }
+            }
+        }
+    }
 
+    private fun renderButtonState(state: ResendButtonState) {
+        when (state) {
+            is ResendButtonState.Enabled -> {
+                viewBinding.repeatCodeTimer.isVisible = false
+                viewBinding.repeatCodeButton.isEnabled = true
+            }
+            is ResendButtonState.TimerTicking -> {
+                viewBinding.repeatCodeTimer.isVisible = true
+                viewBinding.repeatCodeTimer.text =
+                    timeMillisToTimerText(state.timeRemainSeconds)
+                viewBinding.repeatCodeButton.isEnabled = false
+            }
+        }
+    }
+
+    private fun timeMillisToTimerText(secondsRemain: Long): String {
+        val minutes = with(secondsRemain / 60) {
+            "${this / 10}${this % 10}"
+        }
+        val seconds = with(secondsRemain % 60) {
+            "${this / 10}${this % 10}"
+        }
+        return "${minutes}:${seconds}"
+    }
 }
--- app/src/main/java/com/postnikovegor/mobiledev/ui/emailconfirmation/EmailConfirmationViewModel.kt
+++ app/src/main/java/com/postnikovegor/mobiledev/ui/emailconfirmation/EmailConfirmationViewModel.kt
@@ -1,6 +1,42 @@
 package com.postnikovegor.mobiledev.ui.emailconfirmation
 
+import android.os.CountDownTimer
 import com.postnikovegor.mobiledev.ui.base.BaseViewModel
+import com.postnikovegor.mobiledev.ui.emailconfirmation.EmailConfirmationViewModel.ResendButtonState.Enabled
+import com.postnikovegor.mobiledev.ui.emailconfirmation.EmailConfirmationViewModel.ResendButtonState.TimerTicking
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import java.util.concurrent.TimeUnit
 
 class EmailConfirmationViewModel : BaseViewModel() {
+
+    sealed class ResendButtonState {
+        object Enabled : ResendButtonState()
+        class TimerTicking(val timeRemainSeconds: Long) : ResendButtonState()
+    }
+
+    private val resendButtonStateFlowInner =
+        MutableStateFlow<ResendButtonState>(Enabled)
+
+    val resendButtonStateFlow
+        get() = resendButtonStateFlowInner.asStateFlow()
+
+    private var resendButtonStateValue: ResendButtonState
+        get() = resendButtonStateFlowInner.value
+        set(newValue) {
+            resendButtonStateFlowInner.value = newValue
+        }
+
+    fun notifyResendCodeButtonClicked() {
+        object : CountDownTimer(60000, 1000) {
+            override fun onTick(msRemain: Long) {
+                resendButtonStateValue =
+                    TimerTicking(TimeUnit.MILLISECONDS.toSeconds(msRemain))
+            }
+
+            override fun onFinish() {
+                resendButtonStateValue = Enabled
+            }
+        }.start()
+    }
 }
