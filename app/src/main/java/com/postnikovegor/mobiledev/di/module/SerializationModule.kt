82e4dc792973c70bac957da3c06f8b022cc4d9b5
Egor Postnikov <ea-post134@yandex.ru>
2021-12-08 13:40:35 +0300

    Feature-31: Add dagger modules

---
+++ app/src/main/java/com/postnikovegor/mobiledev/di/module/SerializationModule.kt
@@ -0,0 +1,22 @@
+package com.postnikovegor.mobiledev.di.module
+
+import com.postnikovegor.mobiledev.data.json.AuthTokensAdapter
+import com.postnikovegor.mobiledev.entity.AuthTokens
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SerializationModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(AuthTokens::class.java, AuthTokensAdapter().nullSafe())
+            .build()
+}
